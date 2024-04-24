a7238808c2035e4a14def667a2dc1e529da2c2bd
wrfmartinez <wrmartinez709@gmail.com>
2024-04-24 13:53:04 -0400

    Ensure proper MongoDB connection

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const prompt = require('prompt-sync')();
 const dotenv = require('dotenv')
 dotenv.config();
 const mongoose = require('mongoose');
+
+const connect = async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log('Connected to MongoDB');
+}
+
+connect();
