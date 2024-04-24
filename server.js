09d8e0fc5f5f4df79aa2d845f4c68f9e5cc67fee
wrfmartinez <wrmartinez709@gmail.com>
2024-04-24 14:48:13 -0400

    Create init function to start CRM server

--- server.js
+++ server.js
@@ -4,11 +4,41 @@ dotenv.config();
 const mongoose = require('mongoose');
 const Customer = require('./models/customer');
 
+const init = async () => {
+  console.log("Welcome to the CRM\n");
+  console.log("What would you like to do?\n");
+  console.log("1. Create a customer");
+  console.log("2. View all customers");
+  console.log("3. Update a customer");
+  console.log("4. Delete a customer");
+  console.log("5. Quit\n");
+
+  const userInput = prompt('Enter Number to Run: ');
+
+  if (userInput === "1") {
+    console.clear();
+    name = prompt('Enter a customer name: ');
+    age = prompt('Enter customer age: ');
+    if (name && age) {
+      await createCustomer(name, age);
+      console.clear();
+      console.log('Successfully created a customer');
+    } else {
+      while (!(name && age)) {
+        console.clear();
+        console.log('Failed to create a customer: The name and age is required to create a customer');
+        name = prompt('Enter a customer name: ');
+        age = prompt('Enter customer age: ');
+      }
+    }
+  }
+}
+
 const connect = async () => {
   await mongoose.connect(process.env.MONGODB_URI);
-  console.log('Connected to MongoDB');
+  console.log('Connected to MongoDB\n');
 
-  await runQueries();
+  await init();
 
   await mongoose.disconnect();
   console.log('MongoDB Disconnected');
@@ -45,10 +75,6 @@ const deleteCustomer = async (id) => {
     const customerToDelete = await Customer.findByIdAndDelete(id);
     console.log('Customer successfully deleted');
   }
-}
-
-const runQueries = async () => {
-  console.log('Running queries');
 }
 
 connect();
