9cf7b34979a4a990f725a130338ddfbf1709055d
wrfmartinez <wrmartinez709@gmail.com>
2024-04-24 13:50:00 -0400

    Define customer model and build customer schema

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const prompt = require('prompt-sync')();
-const username = prompt('What is your name? ');
-
-console.log(`Your name is ${username}`);
+const dotenv = require('dotenv')
+dotenv.config();
+const mongoose = require('mongoose');
